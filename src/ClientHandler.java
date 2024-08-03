f258dffb3ea45f0fd2236dfe72f27cf2e3ebf4a0
SarikaRamamoorthy <myothermailid001@gmail.com>
2024-08-03 22:57:42 +0530

    ClientHandler completed

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,10 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ClientHandler implements Runnable {
 
+    static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private String clientUserName;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER " + clientUserName + " has entered the chat!");
+        }catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+    
+    public void broadcastMessage(String messageToSend) {
+        for(ClientHandler clientHandler : clientHandlers) {
+            try {
+                if(!clientHandler.clientUserName.equals(clientUserName)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER " + clientUserName + " has Left the chat!");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/Server.java
+++ src/Server.java
@@ -16,7 +16,7 @@ public class Server {
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
                 System.out.println("A new client has connected!");
-                ClientHandler clientHandler = new ClientHandler();
+                ClientHandler clientHandler = new ClientHandler(socket);
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             }
